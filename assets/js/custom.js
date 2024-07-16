aa191865293bfd75e28b0b84528d278f75100baf
demon <demon@gmail.com>
2024-07-16 13:24:30 -0700

    changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,38 @@ document.addEventListener("mousemove", function (e) {
   var y = e.clientY;
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
 });
+
+
+
+
+// portfolio
+function filterPortfolio(category) {
+  const items = document.querySelectorAll('.pf-item');
+
+  items.forEach(item => {
+      if (category === 'all') {
+          item.style.display = 'block';
+            requestAnimationFrame(() => {
+                item.style.opacity = '1';
+                item.style.transform = 'scale(1)';
+                item.style.transition = 'opacity 0.3s ease, transform 0.3s ease';
+            });
+      } else {
+          if (item.getAttribute('data-category') === category) {
+              item.style.display = 'block';
+            requestAnimationFrame(() => {
+                item.style.opacity = '1';
+                item.style.transform = 'scale(1)';
+                item.style.transition = 'opacity 0.3s ease, transform 0.3s ease';
+            });
+          } else {
+            item.style.opacity = '0';
+            item.style.transform = 'scale(0.5)';
+            item.style.transition = 'opacity 0.3s ease, transform 0.3s ease';
+            setTimeout(() => {
+                item.style.display = 'none';
+            }, 300);
+          }
+      }
+  });
+}
